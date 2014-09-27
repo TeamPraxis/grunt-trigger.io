7b53fab813947aeba3b918a2cbbca9f8cea11e06
Chris Fang <cshfang@gmail.com>
2014-09-26 17:22:28 -1000

    Attempt to pull u/p out of env and fail if not found

--- tasks/forge_build.js
+++ tasks/forge_build.js
@@ -11,9 +11,12 @@
 module.exports = function (grunt) {
   grunt.registerTask('forge_build', 'Executes forge build command', function () {
     var done = this.async();
+    if (!process.env.TRIGGER_USER || !process.env.TRIGGER_PASSWORD) {
+      grunt.fail.warn('Trigger.io credentials not found');
+    }
     grunt.util.spawn({
       cmd: 'node_modules/grunt-trigger/TriggerToolkit/forge',
-      args: ['build', this.args[0]]
+      args: ['build', this.args[0], '--username', process.env.TRIGGER_USER, '--password', process.env.TRIGGER_PASSWORD]
     }, function (error, result, code) {
       if (error) {
         grunt.fail.warn('forge build failed: ' + error, code);
