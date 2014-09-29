111f6e1387c6e045bd8dce7c3b8bf3b752824494
Chris Fang <cshfang@gmail.com>
2014-09-29 12:36:17 -1000

    Renamed tasks

--- tasks/trigger_build.js
+++ tasks/trigger_build.js
@@ -9,17 +9,20 @@
 'use strict';
 
 module.exports = function (grunt) {
-  grunt.registerTask('forge_run', 'Executes forge run (simulator) command', function () {
+  grunt.registerTask('trigger_build', 'Executes forge build command', function () {
     var done = this.async();
     if (!process.env.TRIGGER_USER || !process.env.TRIGGER_PASSWORD) {
       grunt.fail.warn('Trigger.io credentials not found');
     }
     grunt.util.spawn({
-      cmd: 'node_modules/grunt-trigger/TriggerToolkit/forge',
-      args: ['run', this.args[0], '--username', process.env.TRIGGER_USER, '--password', process.env.TRIGGER_PASSWORD]
+      cmd: 'node_modules/grunt-trigger.io/TriggerToolkit/forge',
+      opts: {
+        cwd: './build/'
+      },
+      args: ['build', this.args[0], '--username', process.env.TRIGGER_USER, '--password', process.env.TRIGGER_PASSWORD]
     }, function (error, result, code) {
       if (error) {
-        grunt.fail.warn('forge run failed: ' + error, code);
+        grunt.fail.warn('forge build failed: ' + error, code);
         done();
         return;
       } else {
--- tasks/trigger_run.js
+++ tasks/trigger_run.js
@@ -9,17 +9,17 @@
 'use strict';
 
 module.exports = function (grunt) {
-  grunt.registerTask('forge_build', 'Executes forge build command', function () {
+  grunt.registerTask('trigger_run', 'Executes forge run (simulator) command', function () {
     var done = this.async();
     if (!process.env.TRIGGER_USER || !process.env.TRIGGER_PASSWORD) {
       grunt.fail.warn('Trigger.io credentials not found');
     }
     grunt.util.spawn({
-      cmd: 'node_modules/grunt-trigger/TriggerToolkit/forge',
-      args: ['build', this.args[0], '--username', process.env.TRIGGER_USER, '--password', process.env.TRIGGER_PASSWORD]
+      cmd: 'node_modules/grunt-trigger.io/TriggerToolkit/forge',
+      args: ['run', this.args[0], '--username', process.env.TRIGGER_USER, '--password', process.env.TRIGGER_PASSWORD]
     }, function (error, result, code) {
       if (error) {
-        grunt.fail.warn('forge build failed: ' + error, code);
+        grunt.fail.warn('forge run failed: ' + error, code);
         done();
         return;
       } else {
