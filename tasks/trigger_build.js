78aa0a3e55cbb748778811f9dfc075154d62ac84
Chris Fang <cshfang@gmail.com>
2014-09-29 14:43:58 -1000

    Added device options during run

--- tasks/trigger_build.js
+++ tasks/trigger_build.js
@@ -9,23 +9,52 @@
 'use strict';
 
 module.exports = function (grunt) {
-  grunt.registerTask('trigger_build', 'Executes forge build command', function () {
+  grunt.registerMultiTask('trigger_build', 'Executes forge build command', function () {
     var done = this.async();
+
+    var options = this.options({
+      buildFolder: './build',
+      platform: 'ios',
+      forgePath: __dirname + '/../TriggerToolkit/'
+    });
+
     if (!process.env.TRIGGER_USER || !process.env.TRIGGER_PASSWORD) {
-      grunt.fail.warn('Trigger.io credentials not found');
+      grunt.warn('Trigger.io credentials not found');
     }
+
+    // Make sure the paths have a trailing slash
+    if (options.buildFolder.substr(-1) !== '/') {
+      options.buildFolder += '/';
+    }
+    if (options.forgePath.length > 0 && options.forgePath.substr(-1) !== '/') {
+      options.forgePath += '/';
+    }
+
+    if (!grunt.file.exists(options.buildFolder)) {
+      grunt.warn('Build directory ' + options.buildFolder + ' does not exist');
+    }
+
+    // Initial validations have passed, execute forge build
+    grunt.log.write('Building application...');
+    var args = [
+      'build', options.platform,
+      '--username', process.env.TRIGGER_USER,
+      '--password', process.env.TRIGGER_PASSWORD
+    ];
+
     grunt.util.spawn({
-      cmd: 'node_modules/grunt-trigger.io/TriggerToolkit/forge',
+      cmd: options.forgePath + 'forge',
       opts: {
-        cwd: './build/'
+        cwd: options.buildFolder
       },
-      args: ['build', this.args[0], '--username', process.env.TRIGGER_USER, '--password', process.env.TRIGGER_PASSWORD]
+      args: args
     }, function (error, result, code) {
       if (error) {
-        grunt.fail.warn('forge build failed: ' + error, code);
+        grunt.warn('Failed: ' + error, code);
         done();
         return;
       } else {
+        grunt.log.writeln('DONE!');
         done();
       }
     });
--- tasks/trigger_run.js
+++ tasks/trigger_run.js
@@ -9,17 +9,58 @@
 'use strict';
 
 module.exports = function (grunt) {
-  grunt.registerTask('trigger_run', 'Executes forge run (simulator) command', function () {
+  grunt.registerMultiTask('trigger_run', 'Executes forge run command', function () {
     var done = this.async();
+
+    var options = this.options({
+      buildFolder: './build',
+      platform: 'ios',
+      device: 'simulator',
+      simulatorfamily: 'iphone',
+      forgePath: __dirname + '/../TriggerToolkit/'
+    });
+
     if (!process.env.TRIGGER_USER || !process.env.TRIGGER_PASSWORD) {
-      grunt.fail.warn('Trigger.io credentials not found');
+      grunt.warn('Trigger.io credentials not found');
     }
+
+    // Make sure the paths have a trailing slash
+    if (options.buildFolder.substr(-1) !== '/') {
+      options.buildFolder += '/';
+    }
+    if (options.forgePath.length > 0 && options.forgePath.substr(-1) !== '/') {
+      options.forgePath += '/';
+    }
+
+    if (!grunt.file.exists(options.buildFolder)) {
+      grunt.warn('Build directory ' + options.buildFolder + ' does not exist');
+    }
+
+    // Initial validations have passed, execute forge build
+    grunt.log.write('Starting application (Press CTRL+C to end)');
+    var args = [
+      'run', options.platform,
+      '--username', process.env.TRIGGER_USER,
+      '--password', process.env.TRIGGER_PASSWORD
+    ];
+
+    // Add ios run settings
+    if (options.platform === 'ios') {
+      args.push('--ios.device');
+      args.push(options.device);
+      args.push('--ios.simulatorfamily');
+      args.push(options.simulatorfamily);
+    }
+    
     grunt.util.spawn({
-      cmd: 'node_modules/grunt-trigger.io/TriggerToolkit/forge',
-      args: ['run', this.args[0], '--username', process.env.TRIGGER_USER, '--password', process.env.TRIGGER_PASSWORD]
+      cmd: options.forgePath + 'forge',
+      opts: {
+        cwd: options.buildFolder
+      },
+      args: args
     }, function (error, result, code) {
       if (error) {
-        grunt.fail.warn('forge run failed: ' + error, code);
+        grunt.warn('Failed: ' + error, code);
         done();
         return;
       } else {
