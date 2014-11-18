88d6d3db8d9af0d99353b0c7b78282c0d9257e13
Justin Zaun <jzaun@teampraxis.com>
2014-11-18 08:56:02 -1000

    Added the ability to run forge package

---
+++ tasks/trigger_package.js
@@ -0,0 +1,72 @@
+/*
+ * grunt-trigger
+ * https://github.com/TeamPraxis/grunt-trigger
+ *
+ * Copyright (c) 2014 Chris Fang
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.registerMultiTask('trigger_package', 'Executes forge package command', function () {
+    var done = this.async();
+
+    // Defaults
+    var options = this.options({
+      buildFolder: './build',
+      platform: 'ios',
+      forgePath: __dirname + '/../TriggerToolkit/'
+    });
+
+    if (!process.env.TRIGGER_USER || !process.env.TRIGGER_PASSWORD) {
+      grunt.warn('Trigger.io credentials not found');
+    }
+
+    // Make sure the paths have a trailing slash
+    if (options.buildFolder.substr(-1) !== '/') {
+      options.buildFolder += '/';
+    }
+    if (options.forgePath.length > 0 && options.forgePath.substr(-1) !== '/') {
+      options.forgePath += '/';
+    }
+
+    if (!grunt.file.exists(options.forgePath)) {
+      grunt.warn('Trigger.io Toolkit not found');
+    }
+
+    if (!grunt.file.exists(options.buildFolder)) {
+      grunt.warn('Build directory ' + options.buildFolder + ' does not exist');
+    }
+
+    // Initial validations have passed, execute forge package
+    grunt.log.write('Building application...');
+    var args = [
+      'package', options.platform,
+      '--username', process.env.TRIGGER_USER,
+      '--password', process.env.TRIGGER_PASSWORD
+    ];
+
+    if (options.profile) {
+      args.push('--ios.profile.provisioning_profile');
+      args.push(options.profile);
+    }
+
+    grunt.util.spawn({
+      cmd: options.forgePath + 'forge',
+      opts: {
+        cwd: options.buildFolder
+      },
+      args: args
+    }, function (error, result, code) {
+      if (error) {
+        grunt.warn('Failed: ' + error, code);
+        done();
+        return;
+      } else {
+        grunt.log.writeln('DONE!');
+        done();
+      }
+    });
+  });
+};
